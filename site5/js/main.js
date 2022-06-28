a0806f1e4440c7a8edf7a1c8635e9821da08c7ec
Mutasim <qmutasim77@gmail.com>
2022-06-29 05:04:19 +0600

    finish

---
+++ site5/js/main.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const navBtnImg = document.querySelector("#nav-btn-img");
+
+navBtn.onclick = () => {
+	if(nav.classList.toggle('open')){
+		navBtnImg.src = "img/btnClose.svg"
+	}else{
+		navBtnImg.src = "img/btn.svg"
+	}
+}
+
+AOS.init();
