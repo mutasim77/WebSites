300b4144981f098185bc393b1cf41959b7856d2e
Mutasim <qmutasim77@gmail.com>
2023-01-04 00:58:29 +0600

    Added Classes

--- site24/js/script.js
+++ site24/js/script.js
@@ -148,7 +148,7 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 
     //? Open modal window using setTImeout;
-    const modalTimerId = setTimeout(openModal, 5000);
+    // const modalTimerId = setTimeout(openModal, 5000);
 
     //* Function for opening modal by scrolling;
     function openModalByScroll() {
@@ -163,4 +163,80 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     //? Open modal when user scrolls to the end of the page;
     window.addEventListener('scroll', openModalByScroll);
+
+    //! Create Cards using classes;
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            this.classes.forEach(className => element.classList.add(className));
+            element.innerHTML =
+                `
+                <img src="${this.src}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `
+
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих
+        овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной
+        ценой и высоким качеством!`,
+        9,
+        '.menu .container',
+        'menu__item',
+        'big' // thats how rest operator work
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        `В меню “Премиум” мы используем не только красивый дизайн упаковки, но
+        и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода
+        в ресторан!`,
+        14,
+        '.menu .container',
+        'menu__item'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие
+        продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное
+        количество белков за счет тофу и импортных вегетарианских стейков. `,
+        21,
+        '.menu .container',
+        'menu__item'
+    ).render();
+
 });
